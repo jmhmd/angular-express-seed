5f1b798240efbdf1f3dd12ff9bb6a2c24c837aa5
jmhmd <jhostetter@gmail.com>
2014-01-12 14:49:53 -0500

    switched to ejs

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var app = module.exports = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
